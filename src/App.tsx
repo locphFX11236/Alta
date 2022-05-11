1e68881c8abe5f6029d37030a39e5c250bd420f0
Loc <locphfx11236@funix.edu.vn>
2022-05-11 15:46:23 +0700

    Base Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
 
 function App() {
+  const GreetData = {
+    string: 'AAAA',
+    number: 10,
+    boolean: true,
+    object: {
+        obj1: 'AAA',
+        obj2: 5,
+        obj3: false
+    },
+    array: [
+    {
+      arr1: 'AA',
+      arr2: 3,
+      arr3: true
+    },
+    {
+      arr1: 'A',
+      arr2: 1,
+      arr3: false
+    }
+  ]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet
+        string={GreetData.string}
+        number={GreetData.number}
+        boolean={GreetData.boolean}
+        object={GreetData.object}
+        array={GreetData.array}
+      />
     </div>
   );
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,48 @@
+type GreetProps = {
+    string: string,
+    number: number,
+    boolean: boolean,
+    object: {
+        obj1: string,
+        obj2: number,
+        obj3: boolean
+    },
+    array: {
+        arr1: string,
+        arr2: number,
+        arr3: boolean
+    }[]
+}; // TS quy định type props
+
+const isTrue = (b: boolean) => {
+    let r;
+    if (b) {
+        r = 'True';
+    } else {
+        r = 'False';
+    };
+    return r
+};
+
+export const Greet = (props: GreetProps) => {
+    return (
+        <div>
+            <h2>Welcome to TS!</h2>
+            <p>1. String: {props.string}</p>
+            <p>2. Number: {props.number}</p>
+            <p>3. Boolean: { isTrue(props.boolean) }</p>
+            <hr/>
+            <p>4. Object:</p>
+            <p> - {props.object.obj1}</p>
+            <p> - {props.object.obj2}</p>
+            <p> - { isTrue(props.object.obj3) }</p>
+            <hr/>
+            <p>5. Array:</p>
+            {props.array.map((array) => {
+                return (
+                    <p key={array.arr1}> - {array.arr1} - {array.arr2} - { isTrue(array.arr3) }</p>
+                )
+            })}
+        </div>
+    )
+};
